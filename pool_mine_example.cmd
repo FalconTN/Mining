7ec7d7d88c52d9c6f9a82e23ff73a96a177c7356
FalconTN <fsff3521@gmail.com>
2025-01-24 00:25:04 +0300

    Update pool_mine_example.cmd

--- pool_mine_example.cmd
+++ pool_mine_example.cmd
@@ -24,5 +24,5 @@ xmrig.exe -o pool.xmrfast.com:9000 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDw
 pause
 
 cd /d "%~dp0"
-xmrig.exe -o pool.xmr.pt:9000 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
+xmrig.exe -o pool.xmr.pt:9000 -u 87REqG9YrLwcPuj3tTNo5o7CNJFLXVimQhgCNjKBXrvj8e5iEta72UVCfPX8ggiNNSd9uyYr4qNZRYNYxagXw93mQT2ohKx -p x
 pause
