e062d7af04b15793e0a677bfd88cd38831adf4c7
FalconTN <fsff3521@gmail.com>
2025-01-24 00:12:42 +0300

    Update rtm_ghostrider_example.cmd

--- rtm_ghostrider_example.cmd
+++ rtm_ghostrider_example.cmd
@@ -17,7 +17,7 @@
 
 cd /d "%~dp0"
 :: Use this command line to connect to non-SSL port
-xmrig.exe -a gr -o raptoreumemporium.com:3008 -u 87G4X85m5QddXWMQFppMkFazJKv56wko8LUwwgkicLKRgZ2rgKNfMcUAR3iDV7MHnLVEgWDhZstkzQue9EnVP2EX2otwB8i -p x
+xmrig.exe -a gr -o raptoreumemporium.com:3008 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
 :: Or use this command line to connect to an SSL port
-:: xmrig.exe -a gr -o rtm.suprnova.cc:4273 --tls -u 87G4X85m5QddXWMQFppMkFazJKv56wko8LUwwgkicLKRgZ2rgKNfMcUAR3iDV7MHnLVEgWDhZstkzQue9EnVP2EX2otwB8i -p x
+:: xmrig.exe -a gr -o rtm.suprnova.cc:4273 --tls -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
 pause
